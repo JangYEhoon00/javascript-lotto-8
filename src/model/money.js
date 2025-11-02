51234d4504fb47fd021559d39e64dee26bc7d8af
yehoon00 <rainer8191@gmail.com>
2025-11-03 00:05:13 +0900

    다른 Pc에서 작업을 하기위한 임시커밋

---
+++ src/model/money.js
@@ -0,0 +1,29 @@
+import Inputs from "../controller/input";
+
+export default class Money {
+  #money;
+  #count;
+  // 입력받은 금액의 유효성 검사
+  // 나머지의 몫이 그럼 전부 로또로
+  // 로또는 사용자가 입력한 수 만큼 구매
+  // 세터 지양의 이유
+  constructor(money, count) {
+    this.#money = money;
+    this.#count = count;
+  }
+
+  #modulo(money, count) {
+    if (money % 1000 != 0) {
+      throw new Error("[ERROR] 로또의 금액 투입은 1,000원 단위 입니다.");
+    }
+    const GAME_MONEY = money / (count * 1000);
+
+    return GAME_MONEY;
+  }
+
+  #restMoney(money, GAME_MONEY) {
+    const REST_MONEY = money - GAME_MONEY;
+
+    return REST_MONEY;
+  }
+}
---
+++ src/model/numbers.js
@@ -0,0 +1,5 @@
+import Inputs from "../controller/input";
+
+export default class Numbers {
+  // 입력받은 문자열을 숫자로 변환
+}
