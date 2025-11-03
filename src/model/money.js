af7b589867687bfa6bbc527064a6e2bed59f31d2
yehoon00 <rainer8191@gmail.com>
2025-11-03 10:25:00 +0900

    refactor<Money> : 검증 기능 분리 및 입력값 숫자로 변경

--- src/model/money.js
+++ src/model/money.js
@@ -3,21 +3,38 @@ import Inputs from "./controller/inputs";
 export default class Money {
   #money;
   #count;
+  #GAME_PRICE = 1000;
   // 입력받은 금액의 유효성 검사
   // 나머지의 몫이 그럼 전부 로또로
   // 로또는 사용자가 입력한 수 만큼 구매
   // 세터 지양의 이유
+
   constructor(money, count) {
-    this.#money = money;
+    const CHANGED_MONEY = this.#changeMoney(money);
+    this.#money = CHANGED_MONEY;
     this.#count = count;
   }
 
-  #modulo(money, count) {
-    const GAME_PRICE = 1000;
+  #changeMoney(money) {
+    const CHANGE_NUM = Number(money);
+    if (isNaN(CHANGE_NUM)) {
+      throw new Error("[ERROR] 금액이 숫자가 아닙니다.");
+    }
 
-    if (money % GAME_PRICE != 0) {
+    return CHANGE_NUM;
+  }
+
+  #validateMoney(money) {
+    if (money % 1000 !== 0) {
       throw new Error("[ERROR] 로또의 금액 투입은 1,000원 단위 입니다.");
     }
+    if (money < 1000) {
+      throw new Error("[ERROR] 로또의 금액 투입은 1,000원 이상 입니다.");
+    }
+    return money;
+  }
+
+  #modulo(money, count) {
     const GAME_MONEY = money / (count * GAME_PRICE);
 
     return GAME_MONEY;
