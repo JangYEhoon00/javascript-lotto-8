8a7c748d677ecd38146af62437a29d1003b2e5f6
yehoon00 <rainer8191@gmail.com>
2025-11-03 10:54:27 +0900

    refactor(Numbers) : 로또 번호 입력값 숫자전환 코드 제거
    로또 번호 입력하는 기능이 필요없는것으로 판단해서 제거하였습니다

--- src/model/numbers.js
+++ src/model/numbers.js
@@ -1,12 +1,3 @@
 import Inputs from "./controller/inputs";
 
-export default class Numbers {
-  // 입력받은 문자열을 숫자로 변환
-  changeToNumber(winningNumber) {
-    const fetchWinningNumber = winningNumber.map((index) => {
-      Number(index);
-    });
-
-    return fetchWinningNumber;
-  }
-}
+export default class Numbers {}
